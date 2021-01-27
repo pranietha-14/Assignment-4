4117c7c317b2791e571950afa7cf64568229dcba
pranietha-14 <77775035+pranietha-14@users.noreply.github.com>
2021-01-27 20:14:49 +0530

    Assignment 4 submitted

---
+++ Assignment 4/assign4que 2.js
@@ -0,0 +1,13 @@
+
+function add(){
+
+var a = Number(document.getElementById("first").value);
+
+var b = Number(document.getElementById("second").value);
+
+var c = a + b;
+
+document.getElementById("answer").value=c;
+
+
+}
